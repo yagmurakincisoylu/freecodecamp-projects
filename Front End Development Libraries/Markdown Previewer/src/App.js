c07a32cdaa650ad98a5c857d10f38224e5cf38d7
Yagmur Akincisoylu <71931384+yagmurakincisoylu@users.noreply.github.com>
2022-11-20 17:19:35 +0100

    first commit

---
+++ Front End Development Libraries/Markdown Previewer/src/App.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import Editor from "./components/Editor";
+import Previewer from "./components/Previewer";
+
+function App() {
+  const [text, setText] = useState({
+    value: `# Welcome to my React Markdown Previewer!
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function add(x, y) {
+    return x + y;
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+    - With different indentation levels.
+      - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
+  });
+
+  return (
+    <div className="app">
+      <Editor text={text} setText={setText}/>
+      <Previewer text={text}/>
+    </div>
+  );
+}
+
+export default App;
