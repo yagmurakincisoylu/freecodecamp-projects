0b2761c1c05df5d64e50aff0a06383d591f47dec
Yagmur Akincisoylu <71931384+yagmurakincisoylu@users.noreply.github.com>
2022-11-21 17:37:46 +0100

    first commit

---
+++ Front End Development Libraries/Drum Machine/src/buttonData.js
@@ -0,0 +1,67 @@
+const buttonData = [
+    {
+      text: "Q",
+      keyCode: 81,
+      id: "heater-1",
+      displayText: "Heater 1",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      text: "W",
+      keyCode: 87,
+      id: "heater-2",
+      displayText: "Heater 2",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      text: "E",
+      keyCode: 69,
+      id: "heater-3",
+      displayText: "Heater 3",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      text: "A",
+      keyCode: 65,
+      id: "heater-4",
+      displayText: "Heater 4",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      text: "S",
+      keyCode: 83,
+      id: "clap",
+      displayText: "Clap",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      text: "D",
+      keyCode: 68,
+      id: "open-hh",
+      displayText: "Open HH",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      text: "Z",
+      keyCode: 90,
+      id: "kick-n-hat",
+      displayText: "Kick n'Hat",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      text: "X",
+      keyCode: 88,
+      id: "kick",
+      displayText: "Kick",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      text: "C",
+      keyCode: 67,
+      id: "closed-hh",
+      displayText: "Closed HH",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ]
+
+export default buttonData;
